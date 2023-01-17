9409c0b9b6f61f22b36d1e974230be017dbeff0a
Khalil Acheche <khalil.acheche@epfl.ch>
2023-01-18 00:09:13 +0100

    added random samples from test df + favicon and description

--- main.py
+++ main.py
@@ -9,18 +9,17 @@ import warnings
 import os
 import time
 import gdown
+from PIL import Image
 
 warnings.filterwarnings("ignore")
+im = Image.open("res/favicon.ico")
+st.set_page_config(
+    page_title="Tweet Sentiment Classifier", page_icon=im
+)  # , layout="wide")
 
 
-@st.cache(
-    hash_funcs={
-        tokenizers.Tokenizer: lambda _: 1,
-        tokenizers.AddedToken: lambda _: 1,
-    },
-    suppress_st_warning=True,
-)
-def load_model(model_params):
+@st.experimental_singleton(show_spinner=False)
+def load_model(_model_params):
     model_path = "models/model_bertweet_large-epoch_1"
     checkFiles = [model_path]
     for path in checkFiles:
@@ -37,32 +36,66 @@ def load_model(model_params):
                     msg.success("Download was successful ✅")
             except Exception as e:
                 msg.error(e)
-    # 1- Select pretrained model parameters: bertweet or x_distil_bert_l6h256
-    deep_learning_modules.bertweet_model_params
-    # 2- BiLSTM on top of bert or just mean (BiLSTMTransferLearningClassifier or TransferLearningClassifier)
+
     dl_model = deep_learning_modules.TransferLearningClassifier
-    # 3- freeze bert model or not (freeze_pretrained true or false)
     freeze_pretrained = False
-    # 4- trained model path
-    model = dl_model(model_params, freeze_pretrained)
+
+    model = dl_model(_model_params, freeze_pretrained)
     device = torch.device("cpu")
     model.load_state_dict(torch.load(model_path, map_location=device))
     model.eval()
     return model
 
 
-device = torch.device("cpu")
-model_params = deep_learning_modules.bertweet_model_params
-model = load_model(model_params)
+@st.experimental_singleton(show_spinner=False)
+def load_test_data():
+    return pd.read_csv("data/test_cleaned.txt")
 
-title = st.text_input("Tweet", "Hey! :)")
-test_df = pd.DataFrame([title], columns=["text"])
-test_set = deep_learning_modules.TokenizedDataset(test_df, model_params, False)
-test_loader = DataLoader(test_set, batch_size=1)
-res = model.get_predictions(test_loader, device)
-st.markdown(
-    "<h1 style='text-align: center; color: black;'>Sentiment:{}</h1>".format(
-        "🙂" if res[0][1] == 1 else "🙁"
-    ),
-    unsafe_allow_html=True,
-)
+
+def get_random_tweet():
+    test_data = load_test_data().sample()["text"].values[0]
+    st.session_state["current_text"] = test_data
+
+
+slate = st.empty()
+body = slate.container()
+with body:
+    # title_image = Image.open("viz/wikispeedia.png")
+
+    st.markdown(
+        "<h1 style='text-align: center; '>Tweets Sentiment Classifier 🐦</h1>",
+        unsafe_allow_html=True,
+    )
+    st.markdown(
+        '<p class="big-font">The task of this model is to predict if a tweet message used to contain a positive 🙂 or negative 🙁 smiley, by considering only the remaining text.<br>Click on the button below to see the output of a random tweet (that the model has never seen during the training phase)</p>',
+        unsafe_allow_html=True,
+    )
+    col1, col2, col3 = st.columns([1.3, 1, 1])
+    with col2:
+        start_button = st.button(
+            "Give me a random tweet!",
+            on_click=get_random_tweet,
+        )
+
+
+def run():
+    device = torch.device("cpu")
+    model_params = deep_learning_modules.bertweet_model_params
+    model = load_model(model_params)
+    if "current_text" not in st.session_state:
+        get_random_tweet()
+    with body:
+        title = st.text_input("Tweet", st.session_state["current_text"])
+        test_df = pd.DataFrame([title], columns=["text"])
+        test_set = deep_learning_modules.TokenizedDataset(test_df, model_params, False)
+        test_loader = DataLoader(test_set, batch_size=1)
+        res = model.get_predictions(test_loader, device)
+        st.markdown(
+            "<h1 style='text-align: center; color: grey;'>Sentiment:{}</h1>".format(
+                "🙂" if res[0][1] == 1 else "🙁"
+            ),
+            unsafe_allow_html=True,
+        )
+
+
+run()
